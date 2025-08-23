9f2bf2105aea916f67f8865040c8602d85187d28
cunzhuang123 <112844158+cunzhuang123@users.noreply.github.com>
2025-08-23 16:45:55 +0800

    提交 忽略不掉的文件

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -37,6 +37,13 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Program Files/LLVM/bin/llvm-objdump.exe")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("D:/Product/Code/VideoEngine/EngineCpp/build/thirdPart/freetype/cmake_install.cmake")
+  include("D:/Product/Code/VideoEngine/EngineCpp/build/thirdPart/glfw-3.4/cmake_install.cmake")
+
+endif()
+
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
